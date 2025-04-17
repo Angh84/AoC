b26cf289c6bb25ebb27301ab11760986b1e6b487
Andreas Hansson <andreashansson84@gmail.com>
2025-04-17 17:23:06 +0200

    Add core interfaces and attributes for solution structure

---
+++ src/AoC.Core/Attributes/SolutionAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace AoC.Core.Attributes;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class SolutionAttribute(int year, int day) : Attribute
+{
+    public int Year { get; } = year;
+    public int Day { get; } = day;
+}
---
+++ src/AoC.Core/Interfaces/ISolution.cs
@@ -0,0 +1,11 @@
+﻿namespace AoC.Core.Interfaces;
+
+public interface ISolution
+{
+    string SolvePartOne(string input);
+    string SolvePartTwo(string input);
+    
+    string TestInput { get; }
+    string ExpectedOutputPartOne { get; }
+    string ExpectedOutputPartTwo { get; }
+}
