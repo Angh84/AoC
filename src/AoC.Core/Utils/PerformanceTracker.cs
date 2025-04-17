1e26a29a6e2664db03b92d01a063d07fc138633a
Andreas Hansson <andreashansson84@gmail.com>
2025-04-17 17:26:11 +0200

    Add PerformanceTracker utility class for timing operations

---
+++ src/AoC.Core/Utils/PerformanceTracker.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace AoC.Core.Utils
+{
+    public class PerformanceTracker
+    {
+        private readonly Stopwatch _stopwatch = new();
+
+        public void Start() => _stopwatch.Start();
+        public void Stop() => _stopwatch.Stop();
+        public void Reset() => _stopwatch.Reset();
+
+        public double ElapsedMilliseconds => _stopwatch.Elapsed.TotalMilliseconds;
+        public double ElapsedSeconds => _stopwatch.Elapsed.TotalSeconds;
+    }
+}
