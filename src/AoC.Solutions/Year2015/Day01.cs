61d66dc0748fc6099f44cdba40e6e0f634a75f3b
Andreas Hansson <andreashansson84@gmail.com>
2025-04-22 14:48:57 +0200

    Add solutions for 2015 Day 01

--- src/AoC.Solutions/Year2015/Day01.cs
+++ src/AoC.Solutions/Year2015/Day01.cs
@@ -1,6 +1,34 @@
-﻿namespace AoC.Solutions.Year2015;
+﻿using AoC.Core.Attributes;
+using AoC.Core.Interfaces;
 
-public class Day01
+namespace AoC.Solutions.Year2015;
+
+[Solution(2015,1)]
+public class Day01: ISolution
 {
-    
+    public string SolvePartOne(string input)
+    {
+        var currentFloor = input.Sum(c => c == '(' ? 1 : -1);
+        return currentFloor.ToString();
+    }
+
+    public string SolvePartTwo(string input)
+    {
+        var currentFloor = 0;
+        for (var i = 0; i<input.Length; i++)
+        {
+            var currentChar = input[i];
+            currentFloor += currentChar == '(' ? 1 : -1;
+            if (currentFloor == -1)
+            {
+                return (i + 1).ToString();
+            }
+        }
+
+        return "No solution found";
+    }
+
+    public string TestInput => "())";
+    public string ExpectedOutputPartOne => "-1";
+    public string ExpectedOutputPartTwo => "3";
 }
